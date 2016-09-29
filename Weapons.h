d4528cd841b46997b5d2a740269d99753fffb0bc
Alexmaster50 <cranstonalex@hotmail.co.uk>
2016-09-29 16:40:02 +0200

    weapon struct

--- Weapons.h
+++ Weapons.h
@@ -8,26 +8,62 @@
 #include <cstdlib>
 #include <math.h>
 #include <string>
-/*
+
 struct WEAPON {
 
-int integer;
+string displayName = "";
+int wUID
+enum WeaponType{Knife = 1, Sword, Spear};
 
-
-
+int rReach;
+int rDamage
+float rMiss = 0.00;
+float rCritC = 0.00;
+int rCritD;
+float rArmorP = 0.00;
 
 
 };
-WEAPON Flame;
+
+
+WEAPON flame;
 flame.integer = 10;
+/* UUID: axxx
+ *       a = 1 - 3
+ *        xxx = 0 - 999
+ */
+// Knives
+WEAPON BeginnerKnife;
+{
+    BeginnerKnife.displayname = " Beginner Knife";
+    BeginnerKnife.wUID = 1001;
+    BeginnerKnife.WeaponType = 1;
+
+    BeginnerKnife.rReach = 0;
+    BeginnerKnife.rDamage = 10;
+    BeginnerKnife.rMiss = 0.05;
+    BeginnerKnife.rCritC = 0.07;
+    BeginnerKnife.rCritD = 0.10;
+    BeginnerKnife.rArmorP = 0.08;
+}
+
+// Swords
+
+
+
+// Spears
+
+
+
+
 int main(){
 
 
-printf("Flame's int is: %d", flame.integer);
+//printf("Flame's int is: %d", flame.integer);
 
 
 getchar();
 getchar();
 getchar();
 };
-*/
+
